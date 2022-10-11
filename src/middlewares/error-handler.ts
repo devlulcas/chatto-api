8201b3b9074c085d47f4e0ba214ec092252b0e7d
devlulcas <lucasalvesrego.contato@gmail.com>
2022-10-10 22:47:49 -0300

    feat: adiciona middleware para lidar com erros http

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "../exceptions/http-error";
+
+export function errorHandler(
+  error: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const code = error.code || 500;
+  const message = error.message ?? "Ocorreu um erro";
+
+  res.status(code).send({
+    status: code,
+    message,
+  });
+}

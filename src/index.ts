47f1843a8b17f53ff94e1587e56ab08cf7f9e824
devlulcas <lucasalvesrego.contato@gmail.com>
2022-10-10 22:50:22 -0300

    feat: rota versionada

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+import "express-async-errors";
+
+import http from "node:http";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { errorHandler } from "./middlewares/error-handler";
+import { router } from "./routes";
+
+const PORT = process.env.PORT ?? 3333;
+
+const app = express();
+
+const server = http.createServer(app);
+
+// CORS
+const acceptedOrigins = process.env.ORIGINS ?? "*";
+
+app.use(
+  cors({
+    origin: acceptedOrigins,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+// JSON Input
+app.use(express.json());
+
+// Cookies
+app.use(cookieParser());
+
+// API v1
+app.use("/api/v1", router);
+
+// Error handling
+app.use(errorHandler);
+
+const init = () => {
+  try {
+    server.listen(PORT, () => {
+      if (process.env.NODE_ENV === "production") {
+        const URL = process.env.URL;
+        console.log(`You can open it at ${URL}/api/v1`);
+      } else {
+        console.log(`You can open it at http://localhost:${PORT}/api/v1`);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+init();

c9672adbf1c94f2ec8f70149e7958e4e1c740c0c
devlulcas <lucasalvesrego.contato@gmail.com>
2022-11-01 07:01:34 -0300

    fix: resolve problema de binding nas rotas

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller";
-import { zValidate } from "../middlewares/zod";
+import { zod } from "../middlewares/zod";
 import { signInSchema, signUpSchema } from "./validators";
 
 const authRouter = Router();
 
 // Sign In
-authRouter.post("/sign-in", zValidate(signInSchema), authController.signIn);
+authRouter.post("/sign-in", zod(signInSchema), (req, res) => {
+  return authController.signIn(req, res);
+});
 
 // Sign Out
-authRouter.post("/sign-up", zValidate(signUpSchema), (req, res) => {
+authRouter.post("/sign-up", zod(signUpSchema), (req, res) => {
   return authController.signUp(req, res);
 });
 
 // Sign Up
-authRouter.post("/sign-out", authController.signOut);
+authRouter.post("/sign-out", (req, res) => {
+  return authController.signOut(req, res);
+});
 
 export { authRouter };

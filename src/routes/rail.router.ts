5cb625282573442f848f59f29db09cf5ae650f97
devlulcas <lucasalvesrego.contato@gmail.com>
2022-11-01 07:02:18 -0300

    feat: cria endpoints válidos para os trilhos

--- src/routes/rail.router.ts
+++ src/routes/rail.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import railController from "../controllers/rail.controller";
 import { is } from "../middlewares/is";
 import { authMiddleware } from "../middlewares/jwt";
+import railController from "../controllers/rail.controller";
 
 const railRouter = Router();
 
@@ -10,14 +10,18 @@ const railRouter = Router();
  *
  * > Devolve apenas as informações básicas das trilhas, não seus tópicos.
  */
-railRouter.get("/", railController.findAll);
+railRouter.get("/", (req, res) => {
+  return railController.findAll(req, res);
+});
 
 /**
  * # Obtém detalhes de uma trilha
  *
  * > Entrega os dados da trilha em especifico.
  */
-railRouter.get("/:id", railController.findOne);
+railRouter.get("/:id", (req, res) => {
+  return railController.findOne(req, res);
+});
 
 /**
  * # Cria uma trilha
@@ -25,18 +29,24 @@ railRouter.get("/:id", railController.findOne);
  * > Para criar uma trilha é necessário prover informações básicas da trilha.
  * > Com uma trilha criada é possível criar tópicos relacionados a ela.
  */
-railRouter.post("/", authMiddleware, is(["admin"]), railController.create);
+railRouter.post("/", authMiddleware, is(["admin"]), (req, res) => {
+  return railController.create(req, res);
+});
 
 /**
  * # Altera as informações básicas de uma trilha
  */
-railRouter.put("/:id", authMiddleware, is(["admin"]), railController.update);
+railRouter.put("/:id", authMiddleware, is(["admin"]), (req, res) => {
+  return railController.update(req, res);
+});
 
 /**
  * # Oculta uma trilha e todos os seus tópicos
  *
  * > Não é uma boa ideia remover ~completamente as trilhas então apenas as ocultamos
  */
-railRouter.delete("/:id", authMiddleware, is(["admin"]), railController.remove);
+railRouter.delete("/:id", authMiddleware, is(["admin"]), (req, res) => {
+  return railController.remove(req, res);
+});
 
 export { railRouter };

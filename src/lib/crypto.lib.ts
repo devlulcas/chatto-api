cce1262f7387b0cae32c994bbd3e341c4e6887f3
devlulcas <lucasalvesrego.contato@gmail.com>
2022-11-10 07:11:55 -0300

    feat: isolamento de métodos externos

---
+++ src/lib/crypto.lib.ts
@@ -0,0 +1,16 @@
+import bcrypt from "bcryptjs";
+
+export interface ICrypto {
+  hashPassword(password: string): Promise<string>;
+  checkPassword(password: string, hashedPassword: string): Promise<boolean>;
+}
+
+export class Crypto implements ICrypto {
+  public async hashPassword(password: string) {
+    return bcrypt.hash(password, 8);
+  }
+
+  public async checkPassword(password: string, hash: string) {
+    return bcrypt.compare(password, hash);
+  }
+}
---
+++ src/lib/token.lib.ts
@@ -0,0 +1,62 @@
+import { SignOptions, sign, VerifyOptions, verify } from "jsonwebtoken";
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import { Payload } from "../types/payload";
+
+export interface ITokenGenerator {
+  createToken(payload: Payload): string;
+  validateToken(token: string): Promise<Payload>;
+}
+
+export class JWT implements ITokenGenerator {
+  private privateKeyPath: string;
+  private privateKey: Buffer;
+  private publicKeyPath: string;
+  private publicKey: Buffer;
+  private expiresIn: string;
+
+  constructor() {
+    this.privateKeyPath = join(__dirname, "../../jwt.key");
+    this.privateKey = readFileSync(this.privateKeyPath);
+    this.expiresIn = process.env.JWT_EXPIRATION || "1h";
+    this.publicKeyPath = join(__dirname, "../../jwt.key.pub");
+    this.publicKey = readFileSync(this.publicKeyPath);
+  }
+
+  /**
+   * Gera um token JWT assinado
+   *
+   * @param payload Objeto com informações de payload para usar depois
+   * @returns Token assinado
+   */
+  createToken(payload: Payload) {
+    const signOptions: SignOptions = {
+      algorithm: "RS256",
+      expiresIn: this.expiresIn,
+    };
+
+    return sign(payload, this.privateKey, signOptions);
+  }
+
+  /**
+   * Válida o token e retorna o objeto de payload. Caso o token seja inválido uma exceção será lançada
+   *
+   * @param token String do token JWT assinado
+   * @returns Uma promise contendo o payload que estava guardado no token
+   */
+  validateToken(token: string) {
+    const verifyOptions: VerifyOptions = {
+      algorithms: ["RS256"],
+    };
+
+    return new Promise<Payload>((resolve, reject) => {
+      verify(token, this.publicKey, verifyOptions, (error, decoded) => {
+        if (error) return reject(error);
+
+        const data = decoded as Payload;
+
+        resolve(data);
+      });
+    });
+  }
+}

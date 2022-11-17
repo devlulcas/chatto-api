ae160a3548e4c6e824e056a36612320642f255db
devlulcas <lucasalvesrego.contato@gmail.com>
2022-11-17 07:19:05 -0300

    feat: erro customizado para problemas de validação

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 import { HttpError } from "../exceptions/http-error";
 
 export function errorHandler(
@@ -7,8 +8,21 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  const code = error.code || 500;
+  const code = error.code >= 400 ? error.code : 500;
   const content = error.message ?? "Ocorreu um erro";
+
+  if (error instanceof ZodError) {
+    const errors = error.errors.map((error) => ({
+      path: error.path.join("."),
+      message: error.message,
+    }));
+
+    return res.status(400).json({ errors });
+  }
+
+  if (code === 500) {
+    console.error(error);
+  }
 
   res.status(code).send({
     status: code,
